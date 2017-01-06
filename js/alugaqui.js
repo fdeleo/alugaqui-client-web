bfb70da2a7c26e8ae74d278e7df89e23038f693b
Felipe De Leo <felipedeleo@gmail.com>
2017-01-06 00:46:12 -0200

    Adds other inputs on JSON
    Adds the 3 inputs now on the JSON

--- js/alugaqui.js
+++ js/alugaqui.js
@@ -1,18 +1,22 @@
-
-
-// 1 - Event from the form submit | 2 - Selection of the ids.value | 3 - Transform to JSON string 
-
-$( "#myId" ); 
+/*
+Alugaqui JavaScript File
+*/
 
 function formJSON() {
-  // Test with 1 input only
+  // Test with 3 sample inputs
 
   var inputKey1 = $("input").eq(0).attr('id');
   var inputValue1 = $("input").eq(0).val();
+  var inputKey2 = $("input").eq(1).attr('id');
+  var inputValue2 = $("input").eq(1).val();
+  var inputKey3 = $("input").eq(2).attr('id');
+  var inputValue3 = $("input").eq(2).val();
   
-  var jsonString = '{' + inputKey1 + ':' + inputValue1 + '}' ;
+  var jsonString = '{' + inputKey1 + ':' + inputValue1
+  + ',' + inputKey2 + ':' + inputValue2 
+  + ',' + inputKey3 + ':' + inputValue3 + '}' ;
+  
   // Testing to validate response
-  
   console.log (jsonString);
 
 };
